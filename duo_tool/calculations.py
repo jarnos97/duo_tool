a44a559adaebbb25546648073f2b5061e581f155
Jarno Smit <51162363+jarnos97@users.noreply.github.com>
2023-12-30 15:48:30 +0100

    Finished basic tool and added custom start date for payments.

--- duo_tool/calculations.py
+++ duo_tool/calculations.py
@@ -7,10 +7,10 @@ Sources:
 - https://www.berekenhet.nl/lenen-en-krediet/studiefinanciering-terugbetalen.html
 - https://www.geld.nl/lenen/service/aflossing-lening-berekenen
 """
+import pandas as pd
 
 from typing import Tuple
-
-import pandas as pd
+from loguru import logger
 
 
 def monthly_compound(original_debt: int, months_passed: int, interest: float) -> float:
@@ -20,6 +20,10 @@ def monthly_compound(original_debt: int, months_passed: int, interest: float) ->
 
 def monthly_payment(debt_after_aanloopfase: float, interest: float, months: int) -> float:
     """ Calculates the monthly payment of a loan based on debt, interest and total duration in months. """
+    # If the interest is 0%, the monthly payment is simply the debt / months
+    if interest == 0:
+        return round(debt_after_aanloopfase/months, 2)
+
     monthly_interest_rate = (1 + interest) ** (1 / 12) - 1
     payment = (
         debt_after_aanloopfase * monthly_interest_rate * (1 + monthly_interest_rate) ** months / (
@@ -30,80 +34,125 @@ def monthly_payment(debt_after_aanloopfase: float, interest: float, months: int)
 
 def aanloopfase(
     start_date: str,
-    total_months: int,
     interest_rate: float,
     original_debt: int,
+    payment_offset: int
 ) -> Tuple[float, pd.DataFrame]:
-    """ Calculate two elements. The debt after the aanloopfase and the dataframe for plotting. """
+    """
+    Calculate two elements. The debt after the aanloopfase and the dataframe for plotting.
+
+    Args:
+        start_date (str): start date of the aanloopfase
+        interest_rate (float): the current interest rate
+        original_debt (int): the original debt in euros
+        payment_offset (int): the payment offset in months
+    """
     # Get months in the 'aanloopfase' first
-    aanloopfase_dates = pd.date_range(start=start_date, freq='MS', periods=24)
+    aanloopfase_dates = pd.date_range(start=start_date, freq='MS', periods=payment_offset)
 
     # Add interest on the debt
-    current_debt = [monthly_compound(original_debt, x, interest_rate) for x in range(24)]
+    current_debt = [monthly_compound(original_debt, x, interest_rate) for x in range(payment_offset)]
     aanloopfase_df = pd.DataFrame(zip(aanloopfase_dates, current_debt), columns=['month', 'debt'])
 
     # Final debt after aanloopfase
-    final_debt = monthly_compound(original_debt, 23, interest_rate)
+    final_debt = monthly_compound(original_debt, payment_offset - 1, interest_rate)
+    logger.info(f'final debt after aanloopfase: {final_debt}')
 
     return final_debt, aanloopfase_df
 
 
 def payment_phase(
+    original_debt: int,
     debt_after_aanloopfase: float,
     interest: float,
-    months: int
-) -> Tuple[float, pd.DataFrame]:
-    # Determine the monthly payment
+    months: int,
+    start_date: str,
+    payment_offset: int
+) -> Tuple[float, float, pd.DataFrame]:
+    """
+
+    Args:
+        original_debt: the original debt in euros
+        debt_after_aanloopfase: the debt after the aanloopfase
+        interest: the current interest rate
+        months: the amount of months to pay off debt
+        start_date: start date of the aanloopfase
+        payment_offset: the payment offset in months
+
+    Returns:
+
+    """
+    # Determine the monthly payment > this would get overwritten if someone does an extra monthly payment
     payment = monthly_payment(debt_after_aanloopfase, interest, months)
 
+    # Total interest paid
+    interest_paid = (payment * months) - original_debt
+
     # Get the debt over time
-    return payment
+    payment_date = pd.to_datetime(start_date) + pd.DateOffset(months=payment_offset)
+    payment_dates = pd.date_range(start=payment_date, freq='MS', periods=months + 1)
+    linear_payment = round(debt_after_aanloopfase / months, 2)
+    current_debt = [(debt_after_aanloopfase - (x * linear_payment)) for x in range(months + 1)]
+    payment_phase_df = pd.DataFrame(zip(payment_dates, current_debt), columns=['month', 'debt'])
+
+    return payment, interest_paid, payment_phase_df
 
 
 def get_inputs(
     years: int,
     start_date: str,
     original_debt: int,
-    interest_perc: float
-):
+    interest_perc: float,
+    custom_payment_date: str = None
+) -> dict:
+    """
+    Gather the debt over time (df), total interest paid and monthly payment based on input.
+    Args:
+        years: amount of years to pay back loan
+        start_date: the start date of the aanloopfase
+        original_debt: the original debt amount in euros
+        interest_perc: the interest percentage
+        custom_payment_date: optional custom payment date in case of paying earlier or later than standard.
+
+    Returns:
+        dict: the debt over time, interest paid, and monthly payment.
+    """
+    # If a custom payment date is set, we calculate the number of months after which the person wants to start paying.
+    payment_offset = 24  # The default offset is two years
+    if custom_payment_date:
+        delta = pd.to_datetime(custom_payment_date).to_period('M') - pd.to_datetime(start_date).to_period('M')
+        payment_offset = delta.n
+        logger.info(f"Payment offset = {payment_offset} months.")
+        assert 0 < payment_offset <= 60, 'Custom payment date must be after start date and no later than 60 months'
+
     # Convert the interest percentage to a rate
     interest_rate = interest_perc / 100
     months = 12 * years
 
     # Get the information for the aanloopfase
-    debt_after_aanloopfase, aanloopfase_df = aanloopfase(start_date, months, interest_rate, original_debt)
+    debt_after_aanloopfase, aanloopfase_df = aanloopfase(start_date, interest_rate, original_debt, payment_offset)
 
     # Get information for the payment phase
-    payment = payment_phase(debt_after_aanloopfase, interest_rate, months)
-    print(payment)
+    payment, interest_paid, payment_phase_df = payment_phase(
+        original_debt, debt_after_aanloopfase, interest_rate, months, start_date, payment_offset
+    )
 
+    # Combine dataframes
+    df = pd.concat([aanloopfase_df, payment_phase_df], axis=0)
 
-    #
-
-    # # Determine the date range
-    # dates = pd.date_range(start=start_date, freq='MS', periods=num_months+1)
-    #
-    # # Get the months-left column
-    # months_left = [i for i in range(420, -1, -1)]
-    #
-    # # Create df
-    # df = pd.DataFrame(zip(dates, months_left), columns=['month', 'months_left'])
-    #
-    # print(df)
+    return {
+        'debt_over_time': df,
+        'total_interest_paid': interest_paid,
+        'monthly_payment': payment
+    }
 
 
 if __name__ == '__main__':
     date = '01-2024'
     y = 35
-    i = 2.56
+    i = 0.0  # 2.56
     debt = 50_000
+    custom_date = '01-2027'
 
-    get_inputs(y, date, debt, i)
-
-
-# if __name__ == '__main__':
-#     lening = 53_000
-#     rente = 0.0256
-#     looptijd = 420
-#
-#     monthly_payment(lening, rente, looptijd)
+    out = get_inputs(y, date, debt, i, custom_date)
+    print(out['debt_over_time'].tail(3))
--- duo_tool/main.py
+++ duo_tool/main.py
@@ -1,39 +1,129 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+
+from calculations import get_inputs
+from loguru import logger
+
+
+def advanced_options():
+    with st.expander("Klik hier voor de uitgebreide opties", expanded=False):
+        st.write(
+            """
+            Met deze uitgebreidere opties heb je meer vrijheid om dingen aan te passen.
+            Je kan bijvoorbeeld ervoor kiezen om al eerder of juist later te beginnen met afbetalen, een
+            maandelijkse of eenmalige extra betaling te doen, of te experimenteren met verschillende rentes
+            door de tijd.
+            """
+        )
+
+        st.write('**5. Later/eerder aflossen**')
+        start_paying_date = st.text_input(
+            'Standaard begin je twee jaar na begin van de aanloopfase met aflossen. Echter, kan je ook al eerder of '
+            'juist later beginnen met aflossen. Uitstellen kan maximaal 5 jaar. \n\n'
+            'Vul niks in als je niet eerder of later wilt gaan aflossen. Vul anders wederom een maand en jaar in.',
+            value=None
+        )
+
+        st.write('**6. Veranderende rente**')
+        st.write('under development')
+
+        st.write('**7. Eenmalige extra aflossing**')
+        st.write('under development')
+
+        st.write('**8. Maandelijks extra aflossen**')
+        st.write('under development')
+
+        st.write('**9. Inkomen meenemen**')
+        st.write('under development')
+    return start_paying_date
 
 
 def app():
-    st.title('DUO Calculator - How fucked are you?')
+    st.set_page_config(layout="wide")
+    st.title('DUO Studielening Tool')
 
-    start_date = st.text_input(
-        "Wanneer begint je aanloopfase? Dus 1 januari na het jaar van afstuderen. "
-        "Type datum als maand-jaar, bijvoorbeeld 01-2024 "
-    )
-    # TODO: add type check
+    st.write('introductie ... ')
 
-    years = st.selectbox(
-        "Select je terugbetalingsregels, in aantal jaren",
-        options=[15, 35]
-    )
+    # with st.expander('Klik hier om de basis tool te verbergen',expanded=True):
+    with st.form("basic form"):
+        st.write(
+            """
+            Vul hieronder je gegevens in en druk op 'klik om te berekenen' om de uitslag te zien.
+            """
+        )
+        st.write("**1. Aanloopfase**")
+        start_date = st.text_input(
+            "Wanneer begint je aanloopfase? Dit is meestal januari na het jaar van afstuderen. "
+            "Type datum als maand-jaar, bijvoorbeeld 01-2024 ",
+            value='01-2024'
+        )
+        logger.info(f'{start_date}')
 
-    initial_interest = st.number_input("Wat is het huidige rente percentage? Bijvoorbeeld 2,56")
-    original_st = st.number_input('Vul je originele schuldbedrag in. Bijvoorbeeld 30000')
+        st.write("**2. Terugbetalingsregeling**")
+        years = st.selectbox(
+            "Selecteer je terugbetalingsregeling, in aantal jaren",
+            options=[15, 35],
+            index=1,
+            help="Verander deze waarde omhoog of omlaag om het effect van de afbetalingstermijn te zien."
+        )
 
-    # IFF someone fills in the 'delay payments by n months' > just override the start date
+        st.write("**3. Rente**")
+        initial_interest = st.number_input(
+            "Wat is het huidige rente percentage? Bijvoorbeeld 2,56",
+            help="Verander deze waarde omhoog of omlaag om het effect op je betaalde rente te zien."
+        )
+
+        st.write("**4. Schuld**")
+        original_debt = st.number_input('Vul je originele schuldbedrag in hele euros in. Bijvoorbeeld 30000', step=5000)
+
+        # we add the advanced options here
+        custom_payment_date = advanced_options()
+
+        submitted = st.form_submit_button('Klik om te berekenen..')
+        if submitted:
+            inputs = get_inputs(years, start_date, original_debt, initial_interest, custom_payment_date)
+
+            c1, c2, c3 = st.columns(3)
+            with c1:
+                st.metric(
+                    label='**Geschat maandbedrag:**',
+                    value=f"€ {inputs['monthly_payment']:,.2f}",
+                    help='Dit is het bedrag dat je elke maand aan DUO betaald.'
+                )
+
+            with c2:
+                st.metric(
+                    label='**Betaalde rente:**',
+                    value=f"€ {inputs['total_interest_paid']:,.2f}",
+                    help='Dit is de totale rente die je over je lening betaald. Berekent als het totaal betaalde '
+                         'bedrag min het originele schuldbedrag.'
+                )
+
+            with c3:
+                st.metric(
+                    label='**Schuld afgelost op:**',
+                    value=inputs['debt_over_time']['month'].iloc[-1].strftime('%m-%Y')
+                )
+
+            st.write('**Schuld door de tijd:**')
+            st.line_chart(
+                inputs['debt_over_time'].rename(columns={'debt': 'schuld', 'month': 'jaar'}),
+                x='jaar', y='schuld', width=800, use_container_width=False
+            )
 
 
-# TODO: idee: gebruik een streamlit form om alles in 1 keer in te vullen. Dan kunnen mensen ook meerdere
-# Forms invullen om situaties te vergelijken. Plot dan de situaties in elkaar.
-# Zorg dat er meer vrijheid is dan bij de andere tool
-# Laat mensen dus zelf een extra aflosbedrag eenmalig zowel als maandelijks invullen
-# Doe uitgestelde betaling en inkomen als extra.
-# VRAAG: hoe wordt maandbedrag uitgerekend? Per jaar. Simpelweg door schuld/tijd over? > 'precies binnen looptijd af te lossen'
-# Voeg toe in hoeveel tijd je de studieschuld hebt afbetaald. Als button.
-# Houdt rekening met inkomen als extra
-# Wettelijk maandbedrag = startschuld + rentebedrag / rest tijd
-# Idee: voeg een chatbot toe
-# TODO: voeg optie toe om zonder aanloopfase te betalen.
+# Desired features:
+# [check] Determine monthly payment
+# [check] Calculate total paid interest
+# [check] Compare 15yrs and 35yrs
+# [check] Compare different debt amounts
+# [check] start paying directly after study (no aanloopfase)
+# [check] postpone paying
+# - Fill in different interest rates over time
+# - one time payment
+# - Extra monthly payment (difficult: compare with other tool to check calculations. Takes a whole other approach.)
+# - Account for income > must calculate the draagkracht
+# - Add a chatbot for questions > would be very interesting for myself.
+
 
 # Calculate some variables
 date_range = 1
@@ -41,8 +131,6 @@ months_left = 1
 interest_range = 1
 current_debt = 1
 legal_monthly_amount = 1  # TODO: add a definition here > from Duo
-
-
 
 if __name__ == '__main__':
     app()
