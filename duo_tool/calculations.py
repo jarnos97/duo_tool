f012d9e71f653e7d0646f78d7f4c95117729fdf5
Jarno Smit <51162363+jarnos97@users.noreply.github.com>
2024-01-21 16:35:12 +0100

    Updated calculations to reflect reality better

--- duo_tool/calculations.py
+++ duo_tool/calculations.py
@@ -6,8 +6,9 @@ Sources:
 - https://duo.nl/particulier/rekenhulp-studiefinanciering.jsp#/nl/terugbetalen/resultaat
 - https://www.berekenhet.nl/lenen-en-krediet/studiefinanciering-terugbetalen.html
 - https://www.geld.nl/lenen/service/aflossing-lening-berekenen
+- https://www.calculator.net/loan-calculator.html
 """
-from typing import Tuple
+import abc
 
 import numpy as np
 import pandas as pd
@@ -17,87 +18,154 @@ from pandas import Timestamp
 from duo_tool.utils import diff_month
 
 
-def monthly_compound(original_debt: int, months_passed: int, interest: float) -> float:
-    """Calculates monthly compounded interest based on original debt, yearly interest, and the months passed."""
-    return round(original_debt * (pow((1 + interest / 12), months_passed)), 2)
+class LoanPhase(abc.ABC):
+    """Each phase should have a calculate method that returns a dataframe"""
+
+    def __init__(self, start_date: Timestamp, interest_rate: float, debt: float):
+        self.start_date = start_date
+        self.interest_rate = interest_rate
+        self.debt = debt
+
+    def calculate(self) -> pd.DataFrame:
+        ...
 
 
-def monthly_interest_rate(interest: float):
-    """Calculates the monthly interest rate, including compounding interest."""
-    return (1 + interest) ** (1 / 12) - 1
+class AanloopPhase(LoanPhase):
+    """Class to calculate the aanloopfase"""
+
+    def __init__(self, start_date: Timestamp, interest_rate: float, debt: float, payment_offset: int):
+        """
+        Args:
+            start_date (str): start date of the aanloopfase
+            interest_rate: The annual interest rate (as a decimal).
+            debt (int): the original debt in euros
+            payment_offset (int): the payment offset in months
+        """
+        super().__init__(start_date, interest_rate, debt)
+        self.payment_offset = payment_offset
+        self.final_debt = None
+        # self.final_debt_plus_one = None
+
+    @staticmethod
+    def _monthly_compound(original_debt: float, months_passed: int, interest: float) -> float:
+        """Calculates monthly compounded interest based on original debt, yearly interest, and the months passed."""
+        return round(original_debt * (pow((1 + interest / 12), months_passed)), 2)
+
+    def calculate(self) -> pd.DataFrame:
+        """
+        Calculate two elements. The debt after the aanloopfase and the dataframe for plotting.
+        """
+        # Get months in the 'aanloopfase' first
+        aanloopfase_dates = pd.date_range(start=self.start_date, freq="MS", periods=self.payment_offset)
+
+        # Add interest on the debt
+        current_debt = [self._monthly_compound(self.debt, x, self.interest_rate) for x in range(self.payment_offset)]
+
+        # Create output dataframe
+        aanloopfase_df = pd.DataFrame(zip(aanloopfase_dates, current_debt), columns=["month", "debt"])
+        aanloopfase_df[["payment", "principal", "interest"]] = 0.0
+
+        # Save the final debt after aanloopfase
+        self.final_debt = aanloopfase_df["debt"].iloc[-1]
+        logger.info(f"final debt after aanloopfase: {self.final_debt}")
+
+        # # For the first month of the payment phase there is one additional compound after the aanloopfase
+        # self.final_debt_plus_one = self._monthly_compound(self.final_debt, 1, self.interest_rate)
+
+        return aanloopfase_df
 
 
-def monthly_payment(remaining_debt: float, interest: float, months: int) -> float:
-    """Calculates the monthly payment of a loan based on debt, interest and total duration in months."""
-    # If the interest is 0%, the monthly payment is simply the debt / months
-    if interest == 0:
-        return round(remaining_debt / months, 2)
+class PaymentPhase(LoanPhase):
+    """Class to calculate the payment phase"""
 
-    monthly_interest_r = monthly_interest_rate(interest)
-    payment = (
-        remaining_debt
-        * monthly_interest_r
-        * (1 + monthly_interest_r) ** months
-        / ((1 + monthly_interest_r) ** months - 1)
-    )
-    return round(payment, 2)
+    def __init__(self, start_date: Timestamp, interest_rate: float, debt: float, months: int):
+        """
+        Args:
+            start_date (str): start date of the aanloopfase
+            interest_rate: The annual interest rate (as a decimal).
+            debt (int): the current debt in euros, after the aanloopfase
+            months: the amount of months to pay off debt
+        """
+        super().__init__(start_date, interest_rate, debt)
+        self.months = months
+        self.payment = None
 
+    @staticmethod
+    def _monthly_payment(remaining_debt: float, interest: float, months: int) -> float:
+        """Calculates the monthly payment of an amortized loan based on debt, interest and total duration in months."""
+        # If the interest is 0%, the monthly payment is simply the debt / months
+        if interest == 0:
+            return round(remaining_debt / months, 2)
 
-def aanloopfase(
-    start_date: Timestamp, interest_rate: float, original_debt: int, payment_offset: int
-) -> Tuple[float, pd.DataFrame]:
-    """
-    Calculate two elements. The debt after the aanloopfase and the dataframe for plotting.
+        # Define i as the monthly interest rate
+        i = interest / 12
+        n = months
+        payment = ((1 + i) ** n * i) / ((1 + i) ** n - 1) * remaining_debt
+        return round(payment, 2)
 
-    Args:
-        start_date (str): start date of the aanloopfase
-        interest_rate (float): the current interest rate
-        original_debt (int): the original debt in euros
-        payment_offset (int): the payment offset in months
-    """
-    # Get months in the 'aanloopfase' first
-    aanloopfase_dates = pd.date_range(start=start_date, freq="MS", periods=payment_offset)
+    def _calculate_amortization(self) -> pd.DataFrame:
+        """
+        Calculate amortization schedule for loan.
 
-    # Add interest on the debt
-    current_debt = [monthly_compound(original_debt, x, interest_rate) for x in range(payment_offset)]
-    aanloopfase_df = pd.DataFrame(zip(aanloopfase_dates, current_debt), columns=["month", "debt"])
-    aanloopfase_df["payment"] = 0.0
+        Returns:
+            amortization_schedule: pandas dataframe containing amortization information for each month.
+        """
+        # Convert annual interest rate to monthly rate
+        monthly_interest_r = self.interest_rate / 12
 
-    # Final debt after aanloopfase
-    final_debt = monthly_compound(original_debt, payment_offset - 1, interest_rate)
-    logger.info(f"final debt after aanloopfase: {final_debt}")
+        # Get the date range
+        payment_dates = pd.date_range(start=self.start_date, freq="MS", periods=self.months)
 
-    return final_debt, aanloopfase_df
+        # Initialize variables
+        remaining_balance = self.debt
+        amortization_schedule = []
 
+        for month in payment_dates:
+            # Calculate interest for the current month
+            interest_payment = round(remaining_balance * monthly_interest_r, 2)
 
-def payment_phase(
-    remaining_debt: float,
-    interest: float,
-    months: int,
-    payment_date: Timestamp,
-) -> Tuple[float, pd.DataFrame]:
-    """
+            # Calculate principal for the current month
+            principal_payment = round(self.payment - interest_payment, 2)
 
-    Args:
-        remaining_debt: the debt after the aanloopfase
-        interest: the current interest rate
-        months: the amount of months to pay off debt
-        payment_date: start date of the payment phase
+            # Update the remaining balance
+            remaining_balance -= principal_payment
 
-    Returns:
-        Tuple: payment amount, payment dataframe
-    """
-    # Determine the monthly payment
-    payment = monthly_payment(remaining_debt, interest, months)
+            # Ensure the remaining balance doesn't go below zero
+            remaining_balance = max(0, remaining_balance)
 
-    # Get the debt over time
-    payment_dates = pd.date_range(start=payment_date, freq="MS", periods=months)
-    linear_payment = round(remaining_debt / months, 2)
-    current_debt = [(remaining_debt - (x * linear_payment)) for x in range(months)]
-    payment_phase_df = pd.DataFrame(zip(payment_dates, current_debt), columns=["month", "debt"])
-    payment_phase_df["payment"] = payment
+            # Create a dictionary for the current month's information
+            month_info = {
+                "month": month,
+                "debt": remaining_balance,
+                "payment": self.payment,
+                "principal": principal_payment,
+                "interest": interest_payment,
+            }
 
-    return payment, payment_phase_df
+            # Append the dictionary to the amortization schedule
+            amortization_schedule.append(month_info)
+
+        return pd.DataFrame(amortization_schedule)
+
+    def calculate(self) -> pd.DataFrame:
+        """
+        Calculate the monthly payment and the amortization schedule
+        """
+        # Determine the monthly payment
+        logger.info(self.debt)
+        self.payment = self._monthly_payment(self.debt, self.interest_rate, self.months)
+        logger.info(f"Monthly payment will be: {self.payment}")
+
+        # # Get the debt over time
+        # payment_dates = pd.date_range(start=self.start_date, freq="MS", periods=self.months)
+        # linear_payment = round(self.debt / self.months, 2)
+        # current_debt = [(self.debt - (x * linear_payment)) for x in range(self.months)]
+        # payment_phase_df = pd.DataFrame(zip(payment_dates, current_debt), columns=["month", "debt"])
+        # payment_phase_df["payment"] = self.payment
+
+        payment_phase_df = self._calculate_amortization()
+
+        return payment_phase_df
 
 
 def get_inputs(
@@ -120,11 +188,15 @@ def get_inputs(
     months = 12 * years
 
     # Get the information for the aanloopfase
-    debt_after_aanloopfase, aanloopfase_df = aanloopfase(start_date, interest_rate, original_debt, payment_offset)
+    aanloopfase = AanloopPhase(start_date, interest_rate, original_debt, payment_offset)
+    aanloopfase_df = aanloopfase.calculate()
+    debt_after_aanloopfase = aanloopfase.final_debt
 
     # Get information for the payment phase
     first_payment_date = start_date + pd.DateOffset(months=payment_offset)
-    payment, payment_phase_df = payment_phase(debt_after_aanloopfase, interest_rate, months, first_payment_date)
+    payment_phase = PaymentPhase(first_payment_date, interest_rate, debt_after_aanloopfase, months)
+    payment_phase_df = payment_phase.calculate()
+    payment = payment_phase.payment
 
     # Combine dataframes
     df = pd.concat([aanloopfase_df, payment_phase_df], axis=0).reset_index(drop=True)
@@ -176,7 +248,9 @@ def one_time_payment(
         months_left = (years * 12) - diff_month(payment_date, start_date + pd.DateOffset(months=payment_offset))
         remaining_debt = df["debt"].iloc[-1]
 
-        updated_payment, rest_df = payment_phase(remaining_debt, interest_rate, months_left, payment_date)
+        payment_phase = PaymentPhase(payment_date, interest_rate, remaining_debt, months_left)
+        rest_df = payment_phase.calculate()
+        updated_payment = payment_phase.payment()
         rest_df = rest_df.drop(index=df.index[0], axis=0)
 
         logger.info(f"months left after extra payment: {months_left}")
@@ -188,6 +262,7 @@ def one_time_payment(
 
         # Calculate the total interest paid
         interest_paid = round(df["payment"].sum() - original_debt, 2)
+        # TODO: this is not correct!! Need to calculate using the monthly amounts * months
 
         return {
             "debt_over_time": df,
@@ -198,29 +273,30 @@ def one_time_payment(
     # If the payment is during the aanloopfase adapt the aanloopfase and then calculate the payment phase
     else:
         # Calculate rest of aanloopfase and combine
-        debt_after_aanloopfase, aanloopfase_df = aanloopfase(
+        aanloopfase = AanloopPhase(
             start_date=payment_date + pd.DateOffset(months=1),  # The month after the additional payment
             interest_rate=interest_rate,
-            original_debt=df["debt"].iloc[idx],  # The updated debt after payment
+            debt=df["debt"].iloc[idx],  # The updated debt after payment
             payment_offset=payment_offset - idx - 1,  # The original offset minus the amount of months that have passed
         )
+        aanloopfase_df = aanloopfase.calculate()
+        debt_after_aanloopfase = aanloopfase.final_debt
         aanloopfase_df = pd.concat([df, aanloopfase_df], axis=0).reset_index(drop=True)
 
         # Calculate the payment phase
         first_payment_date = start_date + pd.DateOffset(months=payment_offset)
-        payment, payment_phase_df = payment_phase(debt_after_aanloopfase, interest_rate, 12 * years, first_payment_date)
+        payment_phase = PaymentPhase(first_payment_date, interest_rate, debt_after_aanloopfase, 12 * years)
+        payment_phase_df = payment_phase.calculate()
 
         # Combine dataframes
         df = pd.concat([aanloopfase_df, payment_phase_df], axis=0).reset_index(drop=True)
 
         # Calculate the total interest paid
         interest_paid = round(df["payment"].sum() - original_debt, 2)
+        # TODO: this is not correct!! Need to calculate using the monthly amounts * months
 
-        return {"debt_over_time": df, "total_interest_paid": interest_paid, "monthly_payment": payment}
+        return {"debt_over_time": df, "total_interest_paid": interest_paid, "monthly_payment": payment_phase.payment}
 
-    # TODO: check payment phase! The first amount of the payment phase should not equal the last of the aanloopfase!!!
-    # TODO: for the above > for the param debt_after_aanloopfase > we have to increase it with the rent before sending
-    # It to the payment phase!! Both here and in the normal get_inputs!
     # TODO: continue here. Add the same functionality for when the additional payment is during the aanloopfase
     # TODO: look into the total interest paid > is this correct. Why does it increase if you make an extra payment later
 
@@ -228,20 +304,22 @@ def one_time_payment(
 if __name__ == "__main__":
     date = pd.to_datetime("01-2024")
     y = 35
-    i = 2.56
-    debt = 30_000
+    i_p = 2.56
+    d = 30_000
     offset = 24  # in months
 
-    out = get_inputs(y, date, debt, i, 24)
+    out = get_inputs(y, date, d, i_p, 24)
+    out_df = out["debt_over_time"]
 
-    out2 = one_time_payment(
-        inputs=out,
-        payment_amount=10_000,
-        payment_date=pd.to_datetime("01-2025"),
-        interest_perc=i,
-        years=y,
-        start_date=date,
-        original_debt=debt,
-        payment_offset=24,
-        current_monthly_payment=out["monthly_payment"],
-    )
+    # out2 = one_time_payment(
+    #     inputs=out,
+    #     payment_amount=10_000,
+    #     payment_date=pd.to_datetime("01-2025"),
+    #     interest_perc=i,
+    #     years=y,
+    #     start_date=date,
+    #     original_debt=debt,
+    #     payment_offset=24,
+    #     current_monthly_payment=out["monthly_payment"],
+    # )
+    # print(out)
